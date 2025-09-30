85c4dea614ba3e0dbc7f9ec759e36cd6ce40ef05
vladimirmitnovitskiy <harrellprmime@gmail.com>
2025-09-30 20:30:51 +0300

    add hw task1

---
+++ src/hw30.09.2025/brackets.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    //вводим строку
+    char str[100];
+    scanf("%s",str);
+
+    //инициализируем перменные для подсчёта скобок
+    int countL; // ")"
+    int countR; // "("
+
+    bool t = true;
+
+    
+    //считаем
+
+    for (int i = 0; i<strlen(str); i++) {
+        if (str[0] == ')'){
+            t = false;
+            break;
+        }
+        if (str[i] == ')'){
+            countL++;
+        }
+        if (str[i] == '('){
+            countR++;
+        }
+    }
+    
+    if (countL == countR and t != false){
+        printf("Баланс\n");
+    }
+    else {
+        printf("не баланс\n");
+    }
+
+
+
+    return 0;
+}
