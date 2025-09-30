78248bd9806a23861d7341ed5d4eeb3a6296fc18
vladimirmitnovitskiy <harrellprmime@gmail.com>
2025-09-30 21:59:05 +0300

    add hw

---
+++ src/hw30.09.2025/countzeroel.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int n;
+
+    printf("Введите размер массива: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    //считаем нули
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 0) {
+            count++;
+        }
+    }
+    printf("Количество нулевых эллементов: %d\n", count);
+
+    return 0;
+}
