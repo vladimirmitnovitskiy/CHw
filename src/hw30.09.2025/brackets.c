2e5bb1a1e66b36175ebc55b87acbfce02d30ae01
vladimirmitnovitskiy <harrellprmime@gmail.com>
2025-11-24 23:18:30 +0300

    WebKit format

--- src/hw30.09.2025/brackets.c
+++ src/hw30.09.2025/brackets.c
@@ -1,42 +1,39 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
-int main(void){
-    //вводим строку
+int main(void)
+{
+    // вводим строку
     char str[100];
-    scanf("%s",str);
+    scanf("%s", str);
 
-    //инициализируем перменные для подсчёта скобок
+    // инициализируем перменные для подсчёта скобок
     int countL = 0; // ")"
     int countR = 0; // "("
 
     bool t = true;
 
-    
-    //считаем
+    // считаем
 
-    for (int i = 0; i<strlen(str); i++) {
-        if (str[0] == ')'){
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[0] == ')') {
             t = false;
             break;
         }
-        if (str[i] == ')'){
+        if (str[i] == ')') {
             countL++;
         }
-        if (str[i] == '('){
+        if (str[i] == '(') {
             countR++;
         }
     }
-    
-    if (countL == countR && t != false){
+
+    if (countL == countR && t != false) {
         printf("Баланс\n");
-    }
-    else {
+    } else {
         printf("не баланс\n");
     }
-
-
 
     return 0;
 }
