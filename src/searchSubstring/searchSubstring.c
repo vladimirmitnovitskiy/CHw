244ba52ba6e1b7bdcd9bdae8b8435528ce82c349
vladimirmitnovitskiy <harrellprmime@gmail.com>
2025-12-22 05:04:46 +0300

    Add initialization, and move the substring search logic to a separate function.

--- src/searchSubstring/searchSubstring.c
+++ src/searchSubstring/searchSubstring.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+int countOcc(char *s, char *s1)
+{
+    int count = 0;
+    while ((s = strstr(s, s1)) != NULL) {
+        count++;
+        s++;
+    }
+    return count;
+}
+
+
 int main(void)
 {
 
-    char s[1000], s1[1000];
+    char s[1000] = {};
+    char s1[1000] = {};
     int count = 0;
 
     // принимаем строки
@@ -17,12 +29,7 @@ int main(void)
     s[strcspn(s, "\n")] = '\0';
     s1[strcspn(s1, "\n")] = '\0';
 
-    // ищем вхождения
-    char* pos = s;
-    while ((pos = strstr(pos, s1)) != NULL) {
-        count++;
-        pos++;
-    }
+    count = countOcc(s, s1);
 
     printf("Количество вхождений: %d\n", count);
 
