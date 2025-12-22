a0e62168b7498400c4990403a5ef12a14f76b2f4
vladimirmitnovitskiy <harrellprmime@gmail.com>
2025-12-22 04:35:52 +0300

    Add initialization of arrays and variables, chang names of functions and variables to match the style guide.

--- src/optimalSort/optimalSort.c
+++ src/optimalSort/optimalSort.c
@@ -3,11 +3,11 @@
 
 int main(void)
 {
-    int arr[100];
-    int original[100];
+    int arr[100] = {};
+    int original[100] = {};
 
     int count = 0;
-    char term;
+    char term = 0;
 
     while (count < 100 && scanf("%d%c", &arr[count], &term) == 2) {
         original[count] = arr[count];
@@ -19,14 +19,14 @@ int main(void)
     if (count == 0)
         return 0;
 
-    quick_sort(arr, 0, count - 1);
+    quickSort(arr, 0, count - 1);
 
-    int count_nums = 0;
+    int countNums = 0;
 
     for (int i = 0; i < count; i++) {
         if (arr[i] != original[i])
-            count_nums++;
+            countNums++;
     }
 
-    return count_nums;
+    return countNums;
 }
